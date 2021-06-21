4fecb244f64284b4237c670ddc8a54ae7545f0b1
王亮 <853829609@qq.com>
2021-06-21 23:42:28 +0800

    init

---
+++ dream-cattle-domain/src/main/java/com/cattle/dream/B.java
@@ -0,0 +1,9 @@
+package com.cattle.dream;
+
+/**
+ * @Author Romeo
+ * @Date 2021/6/21 23:41
+ * @Version 1.0
+ */
+public class B {
+}
---
+++ dream-cattle-start/src/main/java/com/cattle/dream/C.java
@@ -0,0 +1,9 @@
+package com.cattle.dream;
+
+/**
+ * @Author Romeo
+ * @Date 2021/6/21 23:42
+ * @Version 1.0
+ */
+public class C {
+}
